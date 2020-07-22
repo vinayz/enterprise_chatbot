d1c3d2a2b9c901335b6cab1a0ccbd288d1d26a6e
Vinay Yadav <yadavvinay040@gmail.com>
2020-07-22 16:54:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header('Content-Security-Policy', 'frame-ancestors https://*.maxlifeinsurance.com');
     if ('OPTIONS' === req.method) {
         res.send(200);
     } else {
